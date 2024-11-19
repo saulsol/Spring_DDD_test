4cb5ac1bc0da8d09fe2139ae21d223c5df97f74b
saul <dlathf3210@naver.com>
2024-11-19 20:47:11 +0900

    [START]

---
+++ src/main/java/com/example/ddd_spring/DddSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddSpringApplication.class, args);
+    }
+
+}
